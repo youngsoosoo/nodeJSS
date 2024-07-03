478aa602c2802f87f98aac866b79a7893ec1d50d
youngsoosoo <lovepys01@naver.com>
2024-07-03 19:40:56 +0900

    chore: dialect update

--- config/DBConfig.js
+++ config/DBConfig.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: 'mysql'
+  dialect: process.env.DB_USE
 });
 
 module.exports = sequelize;
